b394f1374f748bf19e2b6b4b57b503c221b4f558
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-24 15:48:55 -0700

    Set up auth page forms, submit handler, wrote signUp function in fetch utils and passed email and password from state to SignUp. Log out user working!

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { signUp } from './services/fetch-utils';
 
 export default function AuthPage() {
   const [email, setEmail] = useState('');
@@ -8,18 +9,21 @@ export default function AuthPage() {
     e.preventDefault();
 
     //use the email and password from the form to create a user in supabase
+    const user = await signUp(email, password);
+    console.log(user);
+    
   }
   return (
     <div>
       <h3>Book Worm: the app for avid readers</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
                     email
-          <input type="email" />
+          <input onChange={e => setEmail(e.target.value)} value={email} type="email" />
         </label>
         <label>
                     password
-          <input type="password" />
+          <input onChange={e => setPassword(e.target.value)} value={password} type="password" />
         </label>
         <button>Sign Up</button>
       </form>
---
+++ src/services/fetch-utils.js
@@ -0,0 +1,11 @@
+import { client } from './client';
+
+
+export async function signUp(email, password) {
+  const { user } = await client.auth.signUp({
+    email: email,
+    password: password,
+  });
+
+  return user;
+}
